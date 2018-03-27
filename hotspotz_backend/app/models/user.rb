a13c672a80d231b491cf3fabd20d07a8545959ec
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-27 15:20:07 -0700

    Accept avatar_base in User model

--- hotspotz_backend/app/models/user.rb
+++ hotspotz_backend/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :avatar_base
+  before_validation :parse_avatar_base
+
   validates :name, presence: true, length: { maximum: 25 }
   validates :username, :password,
             presence: true, length: { minimum: 6, maximum: 20 }
@@ -9,6 +12,28 @@ class User < ApplicationRecord
   validates_format_of :email,
                       with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates_format_of :password, with: /(?=.*[A-Z]+)(?=.*[0-9]+).*/
+
+  has_attached_file :avatar,
+                    styles: { small: '64x64', med: '100x100', large: '200x200' }
+  validates_attachment :avatar,
+                       content_type: {
+                         content_type: [
+                           'image/jpg',
+                           'image/jpeg',
+                           'image/png',
+                           'image/gif'
+                         ]
+                       },
+                       size: { in: 0..10.megabytes }
+
+  private
+
+  def parse_avatar_base
+    return unless avatar_base
+    image = Paperclip.io_adapters.for(avatar_base)
+    image.original_filename = "file.#{image.content_type.split('/')[1]}"
+    self.avatar = image
+  end
 
   has_secure_password
 end
