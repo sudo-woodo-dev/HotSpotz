4110813b04c8eb44198cad0e7f3fcf3ab213ceb9
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-27 12:50:36 -0700

    Restore test_helper

--- hotspotz_backend/test/test_helper.rb
+++ hotspotz_backend/test/test_helper.rb
@@ -1,18 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'shoulda-matchers'
-require 'minitest-spec-context'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :minitest
-      with.library :rails
-    end
-  end
 end
