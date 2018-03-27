752156bc5b6f8230ce15524366d0cc1d3c15bacd
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-27 12:50:09 -0700

    Remove testing gems

--- hotspotz_backend/test/models/user_test.rb
+++ hotspotz_backend/test/models/user_test.rb
@@ -3,26 +3,11 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   describe '#User model' do
     context 'when valid user' do
-      @subject = User.new(name: 'Test User',
-                          username: 'test123',
-                          email: 'user@example.com',
-                          password: 'foobared',
-                          password_confirmation: 'foobared')
+      @subject = User.new
 
       it 'has name' do
-        must validate_presence_of(:username), @user
+        must have_valid(:email).when("a@a.com", "foo@bar.com", "dave@abc.io")
       end
     end
-  #   def setup
-  #     @subject = User.new(name: 'Test User',
-  #                         username: 'test123',
-  #                         email: 'user@example.com',
-  #                         password: 'foobared',
-  #                         password_confirmation: 'foobared')
-  #   end
-  #
-  #   def test_username_presence_validation
-  #     must validate_presence_of(:username), @user
-  #   end
   end
 end
