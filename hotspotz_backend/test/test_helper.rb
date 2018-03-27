4f39d791e90c7933550a03cb815ab15b0009037b
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-27 12:01:50 -0700

    Configure shoulda-matchers

--- hotspotz_backend/test/test_helper.rb
+++ hotspotz_backend/test/test_helper.rb
@@ -7,4 +7,20 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      # with.test_framework :rspec
+      with.test_framework :minitest
+      # with.test_framework :minitest_4
+      # with.test_framework :test_unit
+
+      # Choose one or more libraries:
+      with.library :active_record
+      with.library :active_model
+      with.library :action_controller
+      # Or, choose the following (which implies all of the above):
+      with.library :rails
+    end
+  end
 end
