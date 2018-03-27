a2fbf9cea23afca289a17df8c19d10f473239502
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-27 12:41:28 -0700

    Reorganize test structure

--- hotspotz_backend/test/models/user_test.rb
+++ hotspotz_backend/test/models/user_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def test_validation
-    user = User.new
-    assert_must validate_presence_of(:name), user
+  describe '#User model' do
+    context 'when valid user' do
+      @subject = User.new(name: 'Test User',
+                          username: 'test123',
+                          email: 'user@example.com',
+                          password: 'foobared',
+                          password_confirmation: 'foobared')
+
+      it 'has name' do
+        must validate_presence_of(:username), @user
+      end
+    end
+  #   def setup
+  #     @subject = User.new(name: 'Test User',
+  #                         username: 'test123',
+  #                         email: 'user@example.com',
+  #                         password: 'foobared',
+  #                         password_confirmation: 'foobared')
+  #   end
+  #
+  #   def test_username_presence_validation
+  #     must validate_presence_of(:username), @user
+  #   end
   end
 end
