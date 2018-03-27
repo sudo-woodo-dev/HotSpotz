ff47fda46acbfebe6848e780941227f6fa71437a
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-27 13:12:14 -0700

    Add tests for presence validators

--- hotspotz_backend/test/models/user_test.rb
+++ hotspotz_backend/test/models/user_test.rb
@@ -12,4 +12,29 @@ class UserTest < ActiveSupport::TestCase
   test 'should be valid' do
     assert @subject.valid?
   end
+
+  test 'name should be present' do
+    @subject.name = '     '
+    assert_not @subject.valid?
+  end
+
+  test 'username should be present' do
+    @subject.username = '     '
+    assert_not @subject.valid?
+  end
+
+  test 'email should be present' do
+    @subject.email = '     '
+    assert_not @subject.valid?
+  end
+
+  test 'password should be present' do
+    @subject.password = '     '
+    assert_not @subject.valid?
+  end
+
+  test 'password_confirmation should be present' do
+    @subject.password_confirmation = '     '
+    assert_not @subject.valid?
+  end
 end
