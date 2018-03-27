a46662221ca0afdcc79a9f71e1756386073b244a
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-27 15:05:57 -0700

    Update db schema

--- hotspotz_backend/db/schema.rb
+++ hotspotz_backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327204250) do
+ActiveRecord::Schema.define(version: 20180327215540) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20180327204250) do
     t.string "avatar_base"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
