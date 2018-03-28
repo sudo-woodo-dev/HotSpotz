b1cac724b01de1f6772634e05a3883bb2f9dc07f
JackMcCalley <jackmccalley@gmail.com>
2018-03-28 15:02:36 -0700

    markers added, still need info popups

--- hotspotz_frontend/src/components/MapComponents/MapContainer.js
+++ hotspotz_frontend/src/components/MapComponents/MapContainer.js
@@ -14,14 +14,18 @@ const styles = {
 };
 
 export class MapContainer extends React.Component {
+
     constructor(props){
-        super(props)
+        super(props);
         this.state={
-            userLocationCoord: {}
+            showingInfoWindow: true,
+            activeMarker: {},
+            selectedPlace: {}
         }
     }
 
     render() {
+        const {directions} = this.state;
         return (
           <Map
             google={this.props.google}
@@ -33,10 +37,58 @@ export class MapContainer extends React.Component {
                 lng: -117.1611
             }}
             zoom={13}
+            draggable={false}
+            maxZoom={13}
+            minZoom={13}
+            zoomControl={false}
+            streetViewControl={false}
+            mapTypeControl={false}
+            fullscreenControl={false}
             >
-
-            <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
+            <Marker
+                name={'Gaslamp District'}
+                position={{lat: 32.709960, lng: -117.160484}}
+             />
+             <Marker
+                 name={'Marina / Embarcadero'}
+                 position={{lat: 32.712684, lng: -117.170946}}
+              />
+              <Marker
+                  name={'East Village'}
+                  position={{lat: 32.712125, lng: -117.151177}}
+               />
+                <Marker
+                   name={'Little Italy'}
+                   position={{lat: 32.723399, lng: -117.168674}}
+                />
+                <Marker
+                    name={'North Park'}
+                    position={{lat: 32.744298, lng: -117.129502}}
+                 />
+                 <Marker
+                     name={'Hillcrest'}
+                     position={{lat: 32.746665, lng: -117.163903}}
+                  />
+                  <Marker
+                      name={'Coronado'}
+                      position={{lat: 32.691691, lng: -117.179235}}
+                   />
+                   <Marker
+                       name={'Shelter Island Area'}
+                       position={{lat: 32.724563, lng: -117.228456}}
+                    />
+                    <Marker
+                        name={'Ocean Beach'}
+                        position={{lat: 32.749101, lng: -117.247127}}
+                     />
+                     <Marker
+                         name={'Loma Portal'}
+                         position={{lat: 32.740971, lng: -117.219255}}
+                      />
+                      <Marker
+                          name={'Balboa Park'}
+                          position={{lat: 32.731768, lng: -117.145115}}
+                       />
           </Map>
     );
   }
