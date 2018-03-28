f253d0d562aa09e7dbebd2d7e72647949ef6537b
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 15:09:34 -0700

    Create route to profile page

--- hotspotz_frontend/src/components/Content.js
+++ hotspotz_frontend/src/components/Content.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
+import Profile from './Profile';
 
 
 const Content = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
+    <Route exact path="/profile" component={Profile} />
   </Switch>
 );
 
