74f215b46d5a4cf28a3b4ff0a75a4ba84e89c30a
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 10:45:31 -0700

    Test that user is not created with invalid params

--- hotspotz_backend/test/controllers/users_controller_test.rb
+++ hotspotz_backend/test/controllers/users_controller_test.rb
@@ -5,7 +5,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @subject = users(:test)
   end
 
-  test 'creates a user when valid user_params' do
+  test 'creates user when valid user_params' do
     user_params = {
       user: {
         name: 'Test Test',
@@ -20,5 +20,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response 201
     new_user = User.last
     assert_equal new_user.name, 'Test Test'
+  end
+
+  test 'does not create user when invalid user_params' do
+    user_params = {
+      user: {
+        name: 'Test Test',
+        username: 'test123',
+        email: 'test123@test.com',
+        password: 'Password1',
+        password_confirmation: 'DoesNotMatch'
+      }
+    }
+
+    post '/users', params: user_params
+    assert_response 422
   end
 end
