0f34e333176998b10a539c219b824449d9b25906
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 15:57:08 -0700

    Add state

--- hotspotz_frontend/src/components/Profile.js
+++ hotspotz_frontend/src/components/Profile.js
@@ -2,25 +2,38 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import '../css/Profile.css';
 
-const Profile = () => (
-  <Grid>
-    <Row className="show-grid">
-      <Col xs={6} md={4}>
-        <Row>
-          <img id="avatar" src="http://via.placeholder.com/200x200" alt="Avatar" />
-        </Row>
-        <Row id="name">
-          <p>Fake Name</p>
-        </Row>
-        <Row id="username">
-          <p>FakeUsername</p>
-        </Row>
-      </Col>
-      <Col xs={12} md={8}>
-        <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
-      </Col>
-    </Row>
-  </Grid>
-);
+export default class Profile extends Component {
+  constructor() {
+    super();
 
-export default Profile;
+    this.state = {
+      user: {
+        name: 'Fake Name',
+        username: 'fakeUsername',
+        avatar: 'http://via.placeholder.com/200x200'
+      }
+    };
+  }
+  render() {
+    return (
+      <Grid>
+        <Row className="show-grid">
+          <Col xs={6} md={4}>
+            <Row>
+              <img id="avatar" src={this.state.user.avatar} alt="Avatar" />
+            </Row>
+            <Row id="name">
+              <p>{this.state.user.name}</p>
+            </Row>
+            <Row id="username">
+              <p>{this.state.user.username}</p>
+            </Row>
+          </Col>
+          <Col xs={12} md={8}>
+            <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
