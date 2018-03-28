9ed233f0e27fd8c5379712057a22eb89e271196d
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 09:43:03 -0700

    Strong params

--- hotspotz_backend/app/controllers/users_controller.rb
+++ hotspotz_backend/app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :username, :email, :password, :password_confirmation, :avatar_base
+    )
+  end
 end
