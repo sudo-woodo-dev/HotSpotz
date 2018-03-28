ccaef84f3fdb472a8667fc42d3783caa0bc9d863
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 15:31:51 -0700

    Add grid

--- hotspotz_frontend/src/components/Profile.js
+++ hotspotz_frontend/src/components/Profile.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
-export default class Profile extends Component {
-  render() {
-    return (
+const Profile = () => (
+  <Grid>
+    <Row className="show-grid">
+      <Col xs={12} md={8}>
+        <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
+      </Col>
+      <Col xs={6} md={4}>
+        <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+      </Col>
+    </Row>
+  </Grid>
+);
 
-    )
-  }
-}
+export default Profile;
