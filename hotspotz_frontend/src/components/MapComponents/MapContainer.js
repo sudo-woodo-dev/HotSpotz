2ea5f1fc97292916ea31f9780186cdf141173da8
JackMcCalley <jackmccalley@gmail.com>
2018-03-28 15:17:08 -0700

    markers added, show area name when clicked

--- hotspotz_frontend/src/components/MapComponents/MapContainer.js
+++ hotspotz_frontend/src/components/MapComponents/MapContainer.js
@@ -22,13 +22,34 @@ export class MapContainer extends React.Component {
             activeMarker: {},
             selectedPlace: {}
         }
+
+        this.onMarkerClick = this.onMarkerClick.bind(this);
+        this.onMapClicked = this.onMapClicked.bind(this);
     }
+
+    onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
 
     render() {
         const {directions} = this.state;
         return (
           <Map
             google={this.props.google}
+            onClick={this.onMapClicked}
             zoom={12}
             style={styles.map}
             containerStyle={styles.container}
@@ -47,48 +68,67 @@ export class MapContainer extends React.Component {
             >
             <Marker
                 name={'Gaslamp District'}
+                onClick={this.onMarkerClick}
                 position={{lat: 32.709960, lng: -117.160484}}
              />
              <Marker
                  name={'Marina / Embarcadero'}
+                 onClick={this.onMarkerClick}
                  position={{lat: 32.712684, lng: -117.170946}}
               />
               <Marker
                   name={'East Village'}
+                  onClick={this.onMarkerClick}
                   position={{lat: 32.712125, lng: -117.151177}}
                />
                 <Marker
                    name={'Little Italy'}
+                   onClick={this.onMarkerClick}
                    position={{lat: 32.723399, lng: -117.168674}}
                 />
                 <Marker
                     name={'North Park'}
+                    onClick={this.onMarkerClick}
                     position={{lat: 32.744298, lng: -117.129502}}
                  />
                  <Marker
                      name={'Hillcrest'}
+                     onClick={this.onMarkerClick}
                      position={{lat: 32.746665, lng: -117.163903}}
                   />
                   <Marker
                       name={'Coronado'}
+                      onClick={this.onMarkerClick}
                       position={{lat: 32.691691, lng: -117.179235}}
                    />
                    <Marker
                        name={'Shelter Island Area'}
+                       onClick={this.onMarkerClick}
                        position={{lat: 32.724563, lng: -117.228456}}
                     />
                     <Marker
                         name={'Ocean Beach'}
+                        onClick={this.onMarkerClick}
                         position={{lat: 32.749101, lng: -117.247127}}
                      />
                      <Marker
                          name={'Loma Portal'}
+                         onClick={this.onMarkerClick}
                          position={{lat: 32.740971, lng: -117.219255}}
                       />
                       <Marker
                           name={'Balboa Park'}
+                          onClick={this.onMarkerClick}
                           position={{lat: 32.731768, lng: -117.145115}}
                        />
+
+               <InfoWindow
+                marker={this.state.activeMarker}
+                visible={this.state.showingInfoWindow}>
+                  <div>
+                    <h1>{this.state.selectedPlace.name}</h1>
+                  </div>
+              </InfoWindow>
           </Map>
     );
   }
