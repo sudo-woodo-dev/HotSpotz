dfdf56f7f279c39572837eea339bd48896958e13
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 11:08:48 -0700

    Update show method to render json

--- hotspotz_backend/app/controllers/users_controller.rb
+++ hotspotz_backend/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find params[:id]
+    user = User.find params[:id]
+    render json: user, status: 201
   end
 
   def create
