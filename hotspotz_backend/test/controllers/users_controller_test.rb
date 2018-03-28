7b37691425aba65eaa9d2c742fcf5d919f7cbbea
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 10:34:50 -0700

    Add user creation test

--- hotspotz_backend/test/controllers/users_controller_test.rb
+++ hotspotz_backend/test/controllers/users_controller_test.rb
@@ -4,4 +4,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @subject = users(:test)
   end
+
+  test 'creates a user' do
+    user_params = {
+      user: {
+        name: 'Test Test',
+        username: 'test123',
+        email: 'test123@test.com',
+        password: 'Password1',
+        password_confirmation: 'Password1'
+      }
+    }
+
+    post '/users', params: user_params
+    new_user = User.last
+    assert_equal new_user.name, 'Test Test'
+  end
 end
