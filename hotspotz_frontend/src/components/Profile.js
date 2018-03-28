db29167dbeb9e75b4c164206416128625aab398a
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 15:37:50 -0700

    Add css file for profile component

--- hotspotz_frontend/src/components/Profile.js
+++ hotspotz_frontend/src/components/Profile.js
@@ -4,11 +4,11 @@ import { Grid, Row, Col } from 'react-bootstrap';
 const Profile = () => (
   <Grid>
     <Row className="show-grid">
-      <Col xs={12} md={8}>
-        <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
-      </Col>
       <Col xs={6} md={4}>
         <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+      </Col>
+      <Col xs={12} md={8}>
+        <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
       </Col>
     </Row>
   </Grid>
