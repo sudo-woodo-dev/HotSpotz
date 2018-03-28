96279d4fdd3485eec420903b9cd660bbe9333e37
stephykoo <32823875+stephykoo@users.noreply.github.com>
2018-03-28 14:47:50 -0700

    Updated Enzyme to 3.3.0 version, made a test folder and started creating tests

--- hotspotz_frontend/src/__tests__/App.js
+++ hotspotz_frontend/src/__tests__/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../components/App';
+import { mount } from 'enzyme'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// it("renders create a login form", ()=>{
+//   const app = mount(<Login />)
+//   expect(app.find('.subtitle').text()).toEqual('Add a Cat')
+// })
