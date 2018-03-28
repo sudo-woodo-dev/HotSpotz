1fabdeb32a2a8fdff6d5e6e9102b870dafd201dc
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-28 15:41:59 -0700

    Display user avatar

--- hotspotz_frontend/src/components/Profile.js
+++ hotspotz_frontend/src/components/Profile.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import '../css/Profile.css';
 
 const Profile = () => (
   <Grid>
     <Row className="show-grid">
       <Col xs={6} md={4}>
-        <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+        <Row>
+          <img id="avatar" src="http://via.placeholder.com/200x200" alt="Avatar" />
+        </Row>
       </Col>
       <Col xs={12} md={8}>
         <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
