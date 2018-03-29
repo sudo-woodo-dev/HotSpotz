fc2ed0726eca98ecb0907893beb9402febfe589a
stephykoo <32823875+stephykoo@users.noreply.github.com>
2018-03-29 15:13:29 -0700

    Finished with general review form page

--- hotspotz_frontend/src/components/Content.js
+++ hotspotz_frontend/src/components/Content.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
-import Home from './Home.js';
+import Home from './Home';
+import NewReview from './NewReview'
 
 
 const Content = () => (
   <Switch>
-    <Route exact path="/login" component={Login} />
-	<Route exact path="/" component={Home} />
+  <Route exact path="/" component={Home} />
+  <Route exact path="/login" component={Login} />
+  <Route exact path="/newreview" component={NewReview} />
   </Switch>
 );
 
