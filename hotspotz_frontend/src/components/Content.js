df7a2c677e25c04a5a2dd8601fcb60769a36f35b
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-29 14:11:20 -0700

    Add route to area page

--- hotspotz_frontend/src/components/Content.js
+++ hotspotz_frontend/src/components/Content.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home.js';
-
+import Area from './Area';
 
 const Content = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
 	<Route exact path="/" component={Home} />
+    <Route exact path="/area" component={Area} />
   </Switch>
 );
 
