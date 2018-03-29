14400205eb7d9344ff2af9ad7d01d9faabae8e70
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-29 14:36:33 -0700

    Add fake area data to state

--- hotspotz_frontend/src/components/Area.js
+++ hotspotz_frontend/src/components/Area.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react';
 export default class Area extends Component {
   constructor() {
     super();
+    this.state = {
+      area: {
+        name: 'Fake Area',
+        overallScore: '2.75',
+        parkingScore: '3',
+        cleanlinessScore: '2',
+        safetyScore: '3',
+        affordabilityScore: '3',
+        familyFriendliness: true
+      }
+    };
   }
 
   render() {
