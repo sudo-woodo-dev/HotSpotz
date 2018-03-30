59f5369514345adc3d697f5731d28e688a5dc587
JackMcCalley <jackmccalley@gmail.com>
2018-03-30 15:36:00 -0700

    review controller, model, migration files, and tests

--- hotspotz_backend/config/routes.rb
+++ hotspotz_backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[show create], defaults: { format: :json }
+  resources :reviews, only: %i[show create], defaults: { format: :json }
   post 'user_token' => 'user_token#create'
+  post 'review_token' => 'review_token#create'
 end
