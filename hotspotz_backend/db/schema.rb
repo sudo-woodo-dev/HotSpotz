9238af8145a56367e98c53f88c83d14bd752b519
ndamicobarbour@gmail.com <learn@Mac-mini.localdomain>
2018-03-30 15:16:07 -0700

    postgresql

--- hotspotz_backend/db/schema.rb
+++ hotspotz_backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180327215540) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
