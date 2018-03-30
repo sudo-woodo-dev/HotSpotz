6d200607162944bcb4e1acb34a194cddbfb32034
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-30 10:14:59 -0700

    Create test file for Area page

---
+++ hotspotz_frontend/src/components/Area.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import Area from './Area';
+
+describe('<Area />', () => {
+  let component;
+
+  beforeEach(() => {
+    component = mount(<Area />);
+  });
+
+});
