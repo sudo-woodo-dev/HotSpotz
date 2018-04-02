bb3de7cf920989422fabcd8c62136812d69e1fd3
FDR1510 <frankiedean1510@yahoo.com>
2018-04-02 15:55:38 -0700

    made some review seeds

--- hotspotz_backend/db/seeds.rb
+++ hotspotz_backend/db/seeds.rb
@@ -6,47 +6,75 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 area_attributes = [
-{
-id: 1,
-neighborhood:"Marina Embarcadero"
-},
-{
-id: 2,
-neighborhood:"East Village"
-},
-{
-id: 3,
-neighborhood:"Little Italy"
-},
-{
-id: 4,
-neighborhood:"North Park"
-},
-{
-id: 5,
-neighborhood:"Hillcrest"
-},
-{
-id: 6,
-neighborhood:"Gaslamp Quarter"
-},
-{
-id: 7,
-neighborhood:"Coronado"
-},
-{
-id: 8,
-neighborhood:"Shelter Island Area"
-},
-{
-id: 9,
-neighborhood:"Ocean Beach"
-},
-{
-id: 10,
-neighborhood:"Ioma Portal"
-}
+    {
+        id: 1,
+        neighborhood:"Marina Embarcadero"
+        },
+    {
+        id: 2,
+        neighborhood:"East Village"
+        },
+    {
+        id: 3,
+        neighborhood:"Little Italy"
+        },
+    {
+        id: 4,
+        neighborhood:"North Park"
+        },
+    {
+        id: 5,
+        neighborhood:"Hillcrest"
+        },
+    {
+        id: 6,
+        neighborhood:"Gaslamp Quarter"
+        },
+    {
+        id: 7,
+        neighborhood:"Coronado"
+        },
+    {
+        id: 8,
+        neighborhood:"Shelter Island Area"
+        },
+    {
+        id: 9,
+        neighborhood:"Ocean Beach"
+        },
+    {
+        id: 10,
+        neighborhood:"Ioma Portal"
+    }
 ]
 area_attributes.each do |attributes|
   Area.create(attributes)
+end
+
+review_attributes = [
+    {
+        id:1,
+        area: "Gaslamp Quarter",
+        dining: 4,
+        parking: 1,
+        cleanliness:3,
+        safety:4,
+        price:4,
+        family_friendly: false,
+        review_text: "Gaslamp is super duper, you should try it.."
+    },
+    {
+        id:2,
+        area: "Little Italy",
+        dining: 4,
+        parking: 4,
+        cleanliness: 4,
+        safety: 4,
+        price: 4,
+        family_friendly: true,
+        review_text: "Little Italy reminded of my visit in Rome. Super Rad!"
+    }
+]
+review_attributes.each do |attributes|
+  Review.create(attributes)
 end
