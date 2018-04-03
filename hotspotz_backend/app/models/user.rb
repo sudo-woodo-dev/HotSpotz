f3317a4ea0ef5f76b0c80b1b85039613103fd168
stephykoo <32823875+stephykoo@users.noreply.github.com>
2018-04-03 14:11:13 -0700

    uncommented the validation for image/paperclip

--- hotspotz_backend/app/models/user.rb
+++ hotspotz_backend/app/models/user.rb
@@ -16,16 +16,16 @@ class User < ApplicationRecord
 
   has_attached_file :avatar,
                     styles: { small: '64x64', med: '100x100', large: '200x200' }
-  # validates_attachment :avatar,
-  #                      content_type: {
-  #                        content_type: [
-  #                          'image/jpg',
-  #                          'image/jpeg',
-  #                          'image/png',
-  #                          'image/gif'
-  #                        ]
-  #                      },
-  #                      size: { in: 0..10.megabytes }
+  validates_attachment :avatar,
+                       content_type: {
+                         content_type: [
+                           'image/jpg',
+                           'image/jpeg',
+                           'image/png',
+                           'image/gif'
+                         ]
+                       },
+                       size: { in: 0..10.megabytes }
   # The following method is required for user test fixtures to work.
   # It returns the hash digest of a given string.
   def self.digest(string)
