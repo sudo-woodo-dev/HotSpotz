33f2f676f4060b5c983138ebc6af814f86376b5a
Learn <learn@learn09.localdomain>
2018-04-04 11:04:32 -0700

    add logout functionality

--- hotspotz_frontend/src/components/Navigation.js
+++ hotspotz_frontend/src/components/Navigation.js
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import '../css/Navigation.css';
+import withAuth from './withAuth'
+import AuthService from '../services/AuthService'
+import { Redirect } from 'react-router-dom'
 
-const Navigation = () => (
-  <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="/">HotSpotz</a>
-      </Navbar.Brand>
-    </Navbar.Header>
-    <Nav>
-      <div className = "space">
-      <NavItem className = "hello" eventKey={1} href="/register">
-        <b>Register</b>
-      </NavItem>
-      <NavItem className = "hello" eventKey={2} href="/login">
-        Login
-      </NavItem>
-      <NavItem className = "hello2" eventKey={5} href="/profile">
-        Profile
-      </NavItem>
-      <NavItem className = "hello2" eventKey={5} href="/review">
-        Review
-      </NavItem>
-      <NavItem className = "hello2" eventKey={5} href="/newreview">
-        Submit New Review
-      </NavItem>
-      </div>
-    </Nav>
-  </Navbar>
-);
+const Auth = new AuthService()
+
+export default class Navigation extends Component {
+
+  handleLogout(){
+    Auth.logout()
+  }
 
 
-
-export default Navigation;
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">HotSpotz</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <div className = "space">
+          <NavItem className = "hello" eventKey={1} href="/register">
+            <b>Register</b>
+          </NavItem>
+          <NavItem className = "hello" eventKey={2} href="/login">
+            Login
+          </NavItem>
+          <NavItem className = "hello2" eventKey={5} href="/profile">
+            Profile
+          </NavItem>
+          <NavItem className = "hello2" eventKey={5} href="/review">
+            Review
+          </NavItem>
+          <NavItem className = "hello2" eventKey={5} href="/newreview">
+            Submit New Review
+          </NavItem>
+          <NavItem className = "hello2" onClick={this.handleLogout.bind(this)} href={'/login'}>Logout</NavItem>
+          </div>
+        </Nav>
+      </Navbar>
+)
+}
+};
